2571e0b4f4e98af110ffcb0eeea6a9b02b441267
kishanlathiya <kishanlathiya029@gmail.com>
2022-12-18 04:08:26 +0530

    Mirror Wall

--- lib/web_page.dart
+++ lib/web_page.dart
@@ -9,26 +9,29 @@ class WebPage extends StatefulWidget {
 
 class _WebPageState extends State<WebPage> {
   Map web = <String, String>{
-    "images/netflix.png": "https://www.netflix.com/in/",
-    "images/amazon.png":
+    "https://pngimg.com/uploads/netflix/netflix_PNG10.png":
+        "https://www.netflix.com/in/",
+    "http://store-images.s-microsoft.com/image/apps.32357.14618985536919905.4b30e4f3-f7a1-4421-840c-2cc97b10e8e0.6e7a96f1-33ad-4447-8e2b-7de730f9a05e":
         "https://www.primevideo.com/?ref_=dvm_pds_amz_in_as_s_g_148_mkw_swaDaJpSP-dc&mrntrk=pcrid_386559717051_slid__pgrid_82649959887_pgeo_9062189_x__ptid_kwd-360266561005",
-    "images/disney+.png": "https://www.hotstar.com/in",
-    "images/sonyliv.png": "https://www.sonyliv.com/",
+    "https://lh3.googleusercontent.com/TRdxxqZUpdl6Ludd4XxjMwkTYqQ55a-I8L6lEpbcVxZpl1xjfrmlW18v59wz28mj7HwJ":
+        "https://www.hotstar.com/in",
+    "https://www.freelogovectors.net/wp-content/uploads/2021/12/sonyliv-logo-freelogovectors.net_.png":
+        "https://www.sonyliv.com/",
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: const Color(0xff54759e),
         title: const Text("Web View"),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Wrap(
-            direction: Axis.horizontal,
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: web.entries
                 .map(
                   (e) => GestureDetector(
@@ -36,22 +39,28 @@ class _WebPageState extends State<WebPage> {
                       Navigator.of(context).pushNamed("web", arguments: e);
                     },
                     child: Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 7, vertical: 30),
-                      height: 150,
-                      width: 160,
+                      margin: const EdgeInsets.symmetric(vertical: 20),
+                      height: 200,
+                      width: 200,
                       decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(e.key),
-                          ),
-                          color: Colors.lightBlue.withOpacity(0.5),
+                          color: const Color(0xff54759e).withOpacity(0.5),
                           borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(e.key),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 )
                 .toList(),
           ),
-        ],
+        ),
       ),
     );
   }
